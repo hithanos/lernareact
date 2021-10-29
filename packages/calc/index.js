f86f65ed04ad9d8020f34c107dba63c377d86510
kanthidamarla <kanthi.damarla@qapitacorp.com>
2021-10-29 08:24:42 +0530

    created a pet project for lerna

---
+++ packages/calc/index.js
@@ -0,0 +1,5 @@
+var addModule = require("addition");
+var subModule = require("substraction");
+
+console.log("Addition : " + addModule.add(1,4));
+console.log("substraction : " + subModule.sub(4,1));
